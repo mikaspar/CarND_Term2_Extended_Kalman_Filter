bb9284d82a583bc09233c85fa1f5a6d32cfbfb20
mikaspar <36734496+mikaspar@users.noreply.github.com>
2018-07-17 11:32:34 +0200

    Add files via upload

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,7 +3,7 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -24,19 +24,109 @@ void KalmanFilter::Predict() {
   /**
   TODO:
     * predict the state
+
   */
+  x_ = F_ * x_;
+  MatrixXd F_trans = F_.transpose();
+  P_ = F_ * P_ * F_trans + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+	/**
+	TODO:
+	* update the state by using Kalman Filter equations
+	*/
+	VectorXd y = z - (H_ * x_);
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Ht * Si;
+
+	// new state
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	x_ = x_ + (K * y);
+	P_ = (I - K * H_) * P_;
 }
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+	/**
+	TODO:
+	* update the state by using Extended Kalman Filter equations
+	*/
+
+	float x = x_(0);
+	float y = x_(1);
+	float vx = x_(2);
+	float vy = x_(3);
+	float rho = sqrt(x * x + y * y);
+	float theta = atan2(y, x);
+	float ro_dot = (x * vx + y * vy) / rho;
+
+	VectorXd z_pred = VectorXd(3);
+	z_pred << rho, theta, ro_dot;
+
+	VectorXd y_ = z - z_pred;
+
+	// Normalizing the angles
+	while(y_(1) > M_PI){
+		y_(1) -= 2 * M_PI;
+	}
+
+	while(y_(1) < -M_PI){
+		y_(1) += 2 * M_PI;
+	}
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Ht * Si;
+
+	//new state
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	x_ = x_ + (K * y_);
+	P_ = (I - K * H_) * P_;
+}
+P_ = (I - K * H_) * P_;
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  float x      = x_(0);
+  float y      = x_(1);
+  float vx     = x_(2);
+  float vy     = x_(3);
+  float rho    = sqrt(x * x + y * y);
+  float theta  = atan2(y, x);
+  float ro_dot = (x * vx + y * vy) / rho;
+
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho, theta, ro_dot;
+
+  VectorXd y_ = z - z_pred;
+
+	// Normalizing the angles
+	while(y_(1) > M_PI){
+		y_(1) -= 2 * M_PI;
+	}
+
+	while(y_(1) < -M_PI){
+		y_(1) += 2 * M_PI;
+	}
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Ht * Si;
+
+	//new state
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	x_ = x_ + (K * y_);
+    P_ = (I_ - K_ * H_) * P_;
 }
