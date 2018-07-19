dbf5fa51941c516a2cf6dcf8e5039572439165b5
mikaspar <36734496+mikaspar@users.noreply.github.com>
2018-07-19 16:20:15 +0200

    Add files via upload

--- src/tools.cpp
+++ src/tools.cpp
@@ -17,15 +17,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   */
 
 	VectorXd rmse(4);
+
 	rmse << 0, 0, 0, 0;
 
-
-if (estimations.size == ground_truth.size) {
+    if(estimations.empty()) {
+	  cout << "No estimations delivered" << endl;
+      return rmse;
+    } 
+    if (estimations.size != ground_truth.size)	{
+	  cout << "Number of estimations does not fit the number of ground truth values" << endl;	
+	  return rmse;
+	}	
+	
 	//sum of the differences
 	for(unsigned int i=0; i < estimations.size(); ++i){
 
 		VectorXd difference = estimations[i] - ground_truth[i];
-		difference = difference.array()^2;
+		difference = difference.array()*difference.array();
 		rmse += difference;
 	}	
 		
@@ -37,17 +45,6 @@ if (estimations.size == ground_truth.size) {
 
 	//return the result
 	return rmse;
-	
-	
-  } else if(estimations.empty()) {
-	  cout << "No estimations delivered" << endl;
-      return rmse;	
-	
-  } else {
-	  cout << "Number of estimations does not fit the number of ground truth values" << endl;	
-	  return rmse;
-	}	
-	
 
  }
 
